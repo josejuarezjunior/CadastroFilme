2bbc014e78df8d65ba402ec19d2499a3347c6042
Jose Juarez <juarez.junior@outlook.com>
2021-09-06 16:43:06 -0300

    Initial commit

---
+++ CadastroFilme/Data/CadastroFilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CadastroFilme.Models;
+
+namespace CadastroFilme.Data
+{
+    public class CadastroFilmeContext : DbContext
+    {
+        public CadastroFilmeContext (DbContextOptions<CadastroFilmeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CadastroFilme.Models.Studio> Studio { get; set; }
+    }
+}
